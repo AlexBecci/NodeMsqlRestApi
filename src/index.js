ae75cc5d0b615b96e3298732712aec4e996a93e8
alex <beccialex2002@gmail.com>
2024-03-30 04:28:41 +0100

    program structured

--- src/index.js
+++ src/index.js
@@ -3,18 +3,13 @@
 //nuevo 
 import express from 'express'
 //pool
-import { pool } from './db.js';
-
+import employeesRoutes from "./routes/employees.routes.js"
+import indexRoutes from "./routes/index.routes.js"
 const app = express();
 
-app.get('/ping', async (req, res) => {
-    const [result] = await pool.query('SELECT 1 + 1 AS result')
-    res.json(result[0])
-})
-app.get('/employees', (req, res) => res.send('obteniendo empleados'))
-app.post('/employees', (req, res) => res.send('creando empleados'))
-app.put('/employees', (req, res) => res.send('actualizando empleados'))
-app.delete('/employees', (req, res) => res.send('eliminando empleados'))
+
+app.use(employeesRoutes)
+app.use(indexRoutes)
 
 
 app.listen(3000)
