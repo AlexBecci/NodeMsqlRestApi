aa85c9c4b124c851027f556c74df9cb61948b995
alex <beccialex2002@gmail.com>
2024-03-30 04:13:12 +0100

    conection to db

--- index.js
+++ index.js
@@ -2,9 +2,15 @@
 /* const express = require('express') */
 //nuevo 
 import express from 'express'
+//pool
+import { pool } from './db.js';
 
 const app = express();
 
+app.get('/ping', async (req, res) => {
+    const [result] = await pool.query('SELECT 1 + 1 AS result')
+    res.json(result[0])
+})
 app.get('/employees', (req, res) => res.send('obteniendo empleados'))
 app.post('/employees', (req, res) => res.send('creando empleados'))
 app.put('/employees', (req, res) => res.send('actualizando empleados'))
